62acd1bf41e86963ecfc6c26f27edd310589e733
otreci4sgelt0nas <96852897+otreci4sgelt0nas@users.noreply.github.com>
2022-09-16 16:16:49 +0300

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,10 @@
+# percentage calculator by two inputs
+# amount is amount, percentage is percentage 
+# e.g. 5% of $120 = $140.00
+#
+
+
+def percetageCalculator(amount,percentage):
+    x = amount - amount*percentage/100
+    return x
+
